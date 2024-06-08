1474f1149d3208072926c2e2f19f24e6cd73c4d3
farshadmrd <fa.moradi2000@gmail.com>
2024-06-08 18:29:03 +0200

    simple_test

---
+++ simpleTest.py
@@ -0,0 +1,18 @@
+import requests
+
+def test_api_availability(url="http://localhost:8081/hello"):
+    response = requests.get(url)
+    expected_response = "Hello from Config Server!"
+    
+    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+    assert response.text == expected_response, f"Expected response body '{expected_response}', got {response.text}"
+
+if __name__ == "__main__":
+    print("Running tests")
+
+    try:
+        test_api_availability()
+    except AssertionError as e:
+        print(f"Test failed: {e}")
+    else:  
+        print("All tests ran successfully!")
