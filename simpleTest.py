b95f0655dc4d34ad011fd1fec6826c94cea741f1
farshadmrd <fa.moradi2000@gmail.com>
2024-06-14 11:18:34 +0200

    change hello

--- simpleTest.py
+++ simpleTest.py
@@ -2,7 +2,7 @@ import requests
 
 def test_api_availability(url="http://localhost:8081/hello"):
     response = requests.get(url)
-    expected_response = "Hello from Config Server!"
+    expected_response = "Hello"
     
     assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
     assert response.text == expected_response, f"Expected response body '{expected_response}', got {response.text}"
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     try:
         test_api_availability()
     except AssertionError as e:
-        print(f"Test failed: {e}")
+        # print(f"Test failed: {e}")
+        print("Test failed")
+
     else:  
         print("All tests ran successfully!")
